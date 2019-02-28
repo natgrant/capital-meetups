d86f8501f3904a409a61520c7db845d6375a6fea
Nat Grant <email.natgrant@gmail.com>
2019-02-28 16:35:08 +1300

    create event route added

--- server/routes/events.js
+++ server/routes/events.js
@@ -1,33 +1,73 @@
 const express = require("express");
-const db = require("../db/events");
 const router = express.Router();
+
+const {
+  getAllEvents,
+  getEventsByUserId,
+  getEventsByCategory,
+  getOneEvent,
+  createEvent
+} = require("../db/events");
 
 router.use(express.json());
 
 // get /api/v1/meetups
 router.get("/", (req, res) => {
-  db.getAllEvents().then(events => {
-    res.json(events);
-  });
+  getAllEvents()
+    .then(events => {
+      res.json(events);
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json({ error: "Oh no an error" });
+    });
 });
 
 router.get("/events/:id", (req, res) => {
   const userId = req.params.id;
-  db.getEventsByUserId(userId).then(events => {
-    res.json(events);
-  });
+  getEventsByUserId(userId)
+    .then(events => {
+      res.json(events);
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json({ error: "Oh no an error" });
+    });
 });
 
 router.get("/events/:category", (req, res) => {
   const category = req.body.catagory;
-  db.getEventsByCategory(category).then(events => {
-    res.json(events);
-  });
+  getEventsByCategory(category)
+    .then(events => {
+      res.json(events);
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json({ error: "Oh no an error" });
+    });
 });
 
 router.get("/events/:id", (req, res) => {
   const eventId = req.params.id;
-  db.getOneEvent(id);
+  getOneEvent(eventId)
+    .then(event => {
+      res.json(event);
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json({ error: "Oh no an error" });
+    });
+});
+
+router.post("/", (req, res) => {
+  createEvent(req.body)
+    .then(newEvent => {
+      res.json(newEvent);
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json({ error: "Oh no an error" });
+    });
 });
 
 module.exports = router;
