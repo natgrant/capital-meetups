47974d37a830cd0dab470baa45d112f32ac52169
Nat Grant <email.natgrant@gmail.com>
2019-02-28 16:18:33 +1300

    routes added for get events

--- server/routes/events.js
+++ server/routes/events.js
@@ -3,3 +3,31 @@ const db = require("../db/events");
 const router = express.Router();
 
 router.use(express.json());
+
+// get /api/v1/meetups
+router.get("/", (req, res) => {
+  db.getAllEvents().then(events => {
+    res.json(events);
+  });
+});
+
+router.get("/events/:id", (req, res) => {
+  const userId = req.params.id;
+  db.getEventsByUserId(userId).then(events => {
+    res.json(events);
+  });
+});
+
+router.get("/events/:category", (req, res) => {
+  const category = req.body.catagory;
+  db.getEventsByCategory(category).then(events => {
+    res.json(events);
+  });
+});
+
+router.get("/events/:id", (req, res) => {
+  const eventId = req.params.id;
+  db.getOneEvent(id);
+});
+
+module.exports = router;
