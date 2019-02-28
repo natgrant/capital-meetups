9297f277b9e71220f1b5e00499f6e51d84444cd3
alex-h-k <kuan8@hotmail.com>
2019-02-28 17:44:11 +1300

    WIP db-functions

--- server/db/events.js
+++ server/db/events.js
@@ -5,14 +5,74 @@ function getAllEvents(testDb) {
   return db("events").select();
 }
 
-function getEventsByUserId(id, testDb) {
+function getEventsByUserId(userId, testDb) {
   const db = testDb || connection;
   return db("user_event")
     .join("event", "user_event.event_id", "event.id")
-    .where("user_event.user_id", id)
-    .select(events);
+    .where("user_event.user_id", userId)
+    .select(
+      "event.name as name",
+      "event.description as description",
+      "event.location as location",
+      "event.date as date"
+    );
+}
+
+function getEventByCategory(category, testDb) {
+  const db = testDb || connection;
+  return db("events")
+    .where("category", category)
+    .select();
+}
+
+function getOneEvent(id, testDb) {
+  const db = testDb || connection;
+  return db("events")
+    .where("id", id)
+    .first();
+}
+
+function createEvent(newEvent, testDb) {
+  const db = testDb || connection;
+  return db("users")
+    .where("user.id", event.user_id)
+    .first()
+    .then(user => {
+      return db("events")
+        .insert(newEvent)
+        .then(data => {
+          return db("events").select();
+        });
+    });
+}
+
+function deleteEvent(id) {
+  const db = testDb || connection;
+  return db("events")
+    .where("id", id)
+    .del()
+    .then(data => {
+      return db("events").select();
+    });
+}
+
+function removeEventByUserId(userId, eventId) {
+  const db = testDb || connection;
+  return db("user-event")
+    .where(function() {
+      this.where("userID", userId).Where("eventID", eventId);
+    })
+    .del()
+    .then(data => {
+      return db("events").select();
+    });
 }
 module.exports = {
   getAllEvents,
-  getEventsByUserId
+  getEventsByUserId,
+  getEventByCategory,
+  getOneEvent,
+  createEvent,
+  deleteEvent,
+  removeEventByUserId
 };
--- server/db/users.js
+++ server/db/users.js
@@ -1,34 +1,53 @@
-const {generatePasswordHash} = require('../auth/hash')
+const { generatePasswordHash } = require("../auth/hash");
 
-const connection = require('./connection')
+const connection = require("./connection");
 
-function createUser (user_name, email_address, contact_details, password, testDb) {
-  const db = testDb || connection
+function createUser(
+  user_name,
+  email_address,
+  contact_details,
+  password,
+  testDb
+) {
+  const db = testDb || connection;
 
-  return generatePasswordHash(password)
-    .then(hash => {
-      return db('users').insert({user_name, email_address, contact_details, hash})
-    })
+  return generatePasswordHash(password).then(hash => {
+    return db("users").insert({
+      user_name,
+      email_address,
+      contact_details,
+      hash
+    });
+  });
 }
 
-function userExists (user_name, testDb) {
-  const db = testDb || connection
+function userExists(user_name, testDb) {
+  const db = testDb || connection;
 
-  return db('users')
-    .where('user_name', user_name)
-    .then(users => users.length > 0)
+  return db("users")
+    .where("user_name", user_name)
+    .then(users => users.length > 0);
 }
 
-function getUserByUsername (user_name, testDb) {
-  const db = testDb || connection
+function getUserByUsername(user_name, testDb) {
+  const db = testDb || connection;
 
-  return db('users')
-    .where('user_name', user_name)
-    .first()
+  return db("users")
+    .where("user_name", user_name)
+    .first();
+}
+
+function getUsersByEvent(eventId, testDb) {
+  const db = testDb || connection;
+  return db("user_event")
+    .join("users", "users.id", "user_event.user_id")
+    .where("user_event.event_id", eventId)
+    .select("user.name");
 }
 
 module.exports = {
   createUser,
   userExists,
-  getUserByUsername
-}
+  getUserByUsername,
+  getUsersByEvent
+};
