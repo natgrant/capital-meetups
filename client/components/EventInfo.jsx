2766defd3610ab27e64cb23391dcd9fa9f9b91bb
HarryCandlish <hcnd90@gmail.com>
2019-02-28 18:30:29 +1300

    work on eventinfo page design

--- client/components/EventInfo.jsx
+++ client/components/EventInfo.jsx
@@ -9,6 +9,8 @@ export class EventInfo extends Component {
         }
     render(){
         return(
+        <div>
+        <div>
          <div>
              <section className="hero is-primary">
   <div className="hero-body">
@@ -16,10 +18,47 @@ export class EventInfo extends Component {
       <h1 className="title">
         Event
       </h1>
+      <div>
+      <a class="button is-danger joinbutton">Join</a>
+        </div>
     </div>
   </div>
 </section> 
-            {/* <div>
+   <figure className="image is-50x50 eventimage">
+  <img src="/images/coffee.jpg"/>
+    </figure>
+     </div>
+              <div className="tile is-parent basicdetails">
+    <article className="tile is-child box">
+      <div className="content">
+        <p className="title">Event Details</p>
+        <div className="content">
+        <p>host: bob</p><br></br><p>Time/Date: now</p>
+        </div>
+      </div>
+    </article>
+  </div>
+  </div>
+  <div class="tile is-parent description">
+    <article class="tile is-child notification is-success">
+      <div class="content">
+        <p class="title">Description</p>
+        <p class="subtitle">With even more content</p>
+        <div class="content">
+        </div>
+      </div>
+    </article>
+  </div>
+  
+    </div>
+        ) 
+    }
+}
+
+export default EventInfo
+
+
+        {/* <div>
            <ul> 
                <li>
                     <h2>{this.props.name}</h2>
@@ -29,16 +68,4 @@ export class EventInfo extends Component {
                 <li>{this.props.description}</li>
                                 })}
              </ul>
-             </div> */}
-      
-        <div>
-        <a className="button is-large is-fullwidth">Large</a>
-        </div>
-              </div>
-        )
-    }
-}
-
-export default EventInfo
-
-
+       </div> */}
