819813c1cd6dcb2cfc17318bac460d31ee510590
Nat Grant <email.natgrant@gmail.com>
2019-03-01 13:48:03 +1300

    routes for users added

--- server/routes/users.js
+++ server/routes/users.js
@@ -4,6 +4,31 @@ const router = express.Router();
 
 router.use(express.json());
 
+const { getUserByUsername, getUsersByEvent } = require("../db/users");
+
 // GET /api/v1/users
+router.get("/:user_name", (req, res) => {
+  console.log(req.params.user_name);
+  getUserByUsername(req.params.user_name)
+    .then(events => {
+      res.json(events);
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json({ error: "Oh no an error" });
+    });
+});
+
+router.get("/event/:event_id", (req, res) => {
+  console.log(req.params.event_id);
+  getUsersByEvent(req.params.event_id)
+    .then(event => {
+      res.json(event);
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json({ error: "Oh no an error" });
+    });
+});
 
 module.exports = router;
