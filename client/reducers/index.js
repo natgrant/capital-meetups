e4d145dc002e833481771a5255f16bb11e8b121c
alex-h-k <kuan8@hotmail.com>
2019-03-02 11:24:19 +1300

    add subscriptions in combine reducers

--- client/reducers/index.js
+++ client/reducers/index.js
@@ -1,7 +1,7 @@
 import { combineReducers } from "redux";
 
 import auth from "./auth";
-import subscriptions from "./subsriptions";
+import subscriptions from "./subscriptions";
 
 export default combineReducers({
   auth,
