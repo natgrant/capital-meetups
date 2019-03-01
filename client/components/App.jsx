f46acb896c4f908fdf317c2eb0b4e0bcbdbfc541
arpo-deer <a.vandererf@gmail.com>
2019-03-01 15:21:22 +1300

    alexs updates

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -5,13 +5,14 @@ import { connect } from "react-redux";
 import Login from "./Login";
 import Register from "./Register";
 import Home from "./Home";
+import CategoryPage from "./CatergoryPage";
 
 export function App({ auth }) {
   return (
     <Router>
       <React.Fragment>
         <Route exact path="/" component={Home} />
-
+        <Route exact path="/category" component={CategoryPage} />
         <Route exact path="/login" component={Login} />
         <Route path="/register" component={Register} />
       </React.Fragment>
