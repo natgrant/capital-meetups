95de71fe4aed5ed0142e9fbc6422eac931ca0a74
alex-h-k <kuan8@hotmail.com>
2019-03-02 15:36:26 +1300

    delete wrong reducer file

--- client/reducers/index.js
+++ client/reducers/index.js
@@ -2,6 +2,7 @@ import { combineReducers } from "redux";
 
 import auth from "./auth";
 import home from "./home";
+import subscriptions from "./subscriptions";
 
 export default combineReducers({
   auth,
