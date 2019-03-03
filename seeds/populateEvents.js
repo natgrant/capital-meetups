b67be504d21d778be9dc4bce9f73d86f3c6e2aa2
Nat Grant <email.natgrant@gmail.com>
2019-03-03 22:22:30 +1300

    adding more seed data

--- seeds/populateEvents.js
+++ seeds/populateEvents.js
@@ -11,36 +11,64 @@ exports.seed = function(knex, Promise) {
           name: "Coffee Catchups",
           location: "Raglan Roast",
           description:
-            "Meetup with other Wellingtonians for casual conversation and good coffee",
+            "Meetup with other Wellingtonians for casual conversation and good coffee, discuss a myriad of topics, this is a relaxed event and is open to avid coffee fans",
           category: "Coffee",
           date: 1551598786956,
           is_open: "true",
-          type: "Quick catchups",
+          type: "Quick",
           image: "/images/coffee.jpg"
         },
         {
           id: 2,
           user_id: 1,
-          name: "Pottery Making",
-          location: "Wellington Art School",
-          description: "Meetup and play with clay",
-          category: "arts and crafts",
+          name: "Pottery Classes",
+          location: "Wellington Potters Association",
+          description:
+            "Evening class which caters to different levels of competency, Friday night club is where you can come together and share wine, food and tips on pottery making",
+          category: "Arts and Crafts",
           date: 1551314863037,
           is_open: "true",
-          type: "Half day",
-          image: "/defaultimage.jpg"
+          type: "Evening class",
+          image: "/images/pottery.jpg"
         },
         {
           id: 3,
-          user_id: 1,
-          name: "Downhill Mountain Biking",
-          location: "Mt Vic",
-          description: "Downhill mountain biking meetup, round the city",
-          category: "mountain biking",
+          user_id: 2,
+          name: "Mountain Biking Club",
+          location: "Start at Mount Victoria",
+          description:
+            "Meet up with avid mountain bikers in Wellington, starting at the top of Mount Vic and hitting the trails from medium to hard, end the day with a beer or two and some chats",
+          category: "Biking",
           date: 1551314863037,
           is_open: "true",
           type: "Whole day",
-          image: "/defaultimage.jpg"
+          image: "/images/mountainbike.jpg"
+        },
+        {
+          id: 4,
+          user_id: 2,
+          name: "Language Exchange",
+          location: "Wellington Library",
+          description:
+            "Exchange languages with others, currently we have members from China, England and Germany! Come and pratice a language with us, meeting at Level One of the Library",
+          category: "Languages",
+          date: 1551314863037,
+          is_open: "true",
+          type: "Half day",
+          image: "/images/language.jpg"
+        },
+        {
+          id: 5,
+          user_id: 3,
+          name: "Plant Exchange",
+          location: "Frank Kitts Park",
+          description:
+            "Plant exhange for those who are interested, come down to Frank Kitts park and bring some plants which you would like to exchange for other plants - mainly for those who propogate plants to share with others",
+          category: "Plants",
+          date: 1551314863037,
+          is_open: "true",
+          type: "Morning",
+          image: "/images/plant.jpg"
         }
       ]);
     });
