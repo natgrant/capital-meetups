aaf4cd9cb82f12d30f703c62f264fc0bb6619e96
Nat Grant <email.natgrant@gmail.com>
2019-03-04 22:49:33 +1300

    slice working for array

--- client/components/CategoryList.jsx
+++ client/components/CategoryList.jsx
@@ -1,34 +1,59 @@
-import React from "react";
+import React, { Fragment, Component } from "react";
 import { connect } from "react-redux";
 
-class CategoryList extends React.Component {
+class CategoryList extends Component {
   constructor() {
     super();
   }
 
   render() {
     return (
-      <div className="columns">
-        {this.props.categories.map((category, i) => {
-          if (i % 2 == 0) {
+      <Fragment>
+        <div className="columns">
+          {this.props.categories.slice(0, 3).map((category, i) => {
             return (
-              <div
-                className="column category-cont"
-                onClick={() => {
-                  window.location.hash = "#/category";
-                }}
-              >
-                <figure className="image is-256x256 is-inline-block category-image">
-                  <img className="img-radius" src={category.image} />
-                  <h3 className="category-text">
-                    <span>{category.category}</span>
-                  </h3>
-                </figure>
+              <div className="column">
+                <div
+                  key={i}
+                  className="category-cont"
+                  onClick={() => {
+                    window.location.hash = "#/category";
+                  }}
+                >
+                  <figure className="image is-256x256 is-inline-block category-image">
+                    <img className="img-radius" src={category.image} />
+                    <h3 className="category-text">
+                      <span>{category.category}</span>
+                    </h3>
+                  </figure>
+                </div>
               </div>
             );
-          }
-        })}
-      </div>
+          })}
+        </div>
+        <div className="columns">
+          {this.props.categories.slice(3, 6).map((category, i) => {
+            return (
+              <div className="column">
+                <div
+                  key={i}
+                  className="category-cont"
+                  onClick={() => {
+                    window.location.hash = "#/category";
+                  }}
+                >
+                  <figure className="image is-256x256 is-inline-block category-image">
+                    <img className="img-radius" src={category.image} />
+                    <h3 className="category-text">
+                      <span>{category.category}</span>
+                    </h3>
+                  </figure>
+                </div>
+              </div>
+            );
+          })}
+        </div>
+      </Fragment>
     );
   }
 }
