e7fcb6e271794c3de1ef5f9cb0fc900263f31327
Nat Grant <email.natgrant@gmail.com>
2019-03-05 22:23:27 +1300

    bunch of refactoring on css

--- client/components/EventInfo.jsx
+++ client/components/EventInfo.jsx
@@ -13,12 +13,18 @@ export class EventInfo extends Component {
     };
   }
 
+  componentDidMount() {
+    this.props.getUsersByEvent(this.props.match.params.id),
+      this.props.getEvent(this.props.match.params.id);
+  }
+
   toggleTick = () => {
     this.setState({ buttonClicked: true });
   };
-  componentDidMount() {
-    this.props.getUsersByEvent(this.props.match.params.id);
-  }
+
+  handleDate = date => {
+    return new Date(date).toString();
+  };
 
   render() {
     const { selectedEvent, auth } = this.props;
@@ -30,10 +36,12 @@ export class EventInfo extends Component {
       <div>
         <div>
           <div>
-            <section className="hero is-primary">
+            <section className="hero categories-header">
               <div className="hero-body">
                 <div className="container">
-                  <h1 className="title">{selectedEvent.name}</h1>
+                  <h1 className="title eventinfo-title">
+                    {selectedEvent.name}
+                  </h1>
                   <div>
                     <div>
                       <a
@@ -41,6 +49,7 @@ export class EventInfo extends Component {
                         onClick={this.toggleTick}
                       >
                         Join Event
+                        <i class="fas fa-heart" />
                       </a>
                       {buttonClicked ? (
                         <a className="button">
@@ -52,74 +61,75 @@ export class EventInfo extends Component {
                 </div>
               </div>
             </section>
-            <figure className="image is-50x50 eventimage">
-              <img src={selectedEvent.image} />
-            </figure>
+            <div className="container">
+              <figure className="image is-rounded is-inline-block category-image">
+                <img src={selectedEvent.image} />
+              </figure>
+            </div>
+            <div className="tile is-parent basicdetails">
+              <article className="tile is-classNamehild box">
+                <div className="content">
+                  <p className="title">
+                    When: {this.handleDate(selectedEvent.date)}
+                    <br />
+                    Category:{" "}
+                    <a href={`#/categories`}>{selectedEvent.category}</a>
+                  </p>
+                </div>
+              </article>
+            </div>
           </div>
-          <div className="tile is-parent basicdetails">
-            <article className="tile is-classNamehild box">
+          <div className="tile is-parent description">
+            <article className="tile is-child notification">
               <div className="content">
-                <p className="title">
-                  When's it happening: {selectedEvent.date}
-                  <br />
-                  Category: {selectedEvent.category}
-                </p>
+                <p className="title">Description</p>
+
+                <p className="subtitle">{selectedEvent.description}</p>
+                <div className="content" />
               </div>
             </article>
           </div>
-        </div>
-        <div className="tile is-parent description">
-          <article className="tile is-child notification is-success">
-            <div className="content">
-              <p className="title">Description</p>
-
-              <p className="subtitle">{selectedEvent.description}</p>
-              <div className="content" />
+          <div className="tile is-parent locationtile">
+            <div className="tile is-child box">
+              <p className="title">Location</p>
+              <hr />
+              {selectedEvent.location}
             </div>
-          </article>
-        </div>
-        <div className="tile is-parent locationtile">
-          <div className="tile is-child box">
-            <p className="title">Location</p>
-            {/* {events.location} */}
           </div>
-        </div>
-        <div>
           <div>
-            <nav className="panel attendeepanel">
-              <p className="panel-heading">Members</p>
-              <div className="panel-block">
-                <p className="control has-icons-left">
-                  <input
-                    className="input is-small"
-                    type="text"
-                    placeholder="search"
-                  />
-                  <span className="icon is-small is-left">
-                    <i className="fas fa-search" aria-hidden="true" />
-                  </span>
+            <div>
+              <nav className="panel attendeepanel">
+                <p className="panel-heading">Members</p>
+                <div className="panel-block">
+                  <p className="control has-icons-left">
+                    <input
+                      className="input is-small"
+                      type="text"
+                      placeholder="search"
+                    />
+                    <span className="icon is-small is-left">
+                      <i className="fas fa-search" aria-hidden="true" />
+                    </span>
+                  </p>
+                </div>
+                <p className="panel-tabs">
+                  <a className="is-active">Going</a>
                 </p>
-              </div>
-              <p className="panel-tabs">
-                <a className="is-active">Going</a>
-              </p>
-              <a className="panel-block">
-                <span className="panel-icon">
-                  <i className="fas fa-book" aria-hidden="true" />
-                </span>
-                <ul>
-                  {this.props.selectedEventUsers &&
-                    this.props.selectedEventUsers.map(user => {
-                      return <li key={user.name}>{user.name}</li>;
-                    })}
-                </ul>
-              </a>
-            </nav>
+                <a className="panel-block">
+                  <span className="panel-icon">
+                    <i className="fas fa-book" aria-hidden="true" />
+                  </span>
+                  <ul>
+                    {this.props.selectedEventUsers &&
+                      this.props.selectedEventUsers.map(user => {
+                        return <li key={user.name}>{user.name}</li>;
+                      })}
+                  </ul>
+                </a>
+              </nav>
+            </div>
           </div>
         </div>
-        <a className="button" href="/#">
-          Home
-        </a>
       </div>
     );
   }
@@ -128,7 +138,6 @@ export class EventInfo extends Component {
 function mapStateToProps(state) {
   return {
     auth: state.auth,
-    // events: state.home.events,
     selectedEvent: state.home.selectedEvent,
     selectedEventUsers: state.home.selectedEventUsers
   };
