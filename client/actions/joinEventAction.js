ffce100a102e672dcf0a50c9fe1fb61a9468aeaf
HarryCandlish <hcnd90@gmail.com>
2019-03-06 16:12:24 +1300

    join button working

--- client/actions/joinEventAction.js
+++ client/actions/joinEventAction.js
@@ -1,6 +1,5 @@
 import axios from "axios";
-import { getUsersByEvent } from "./getUsersByEvent";
-// import { getUsersByEventApi } from "../api/users";
+import { getUsersByEventApi } from "../api/users";
 
 export function joinEventAction(userId, eventId) {
   console.log("userId", userId);
@@ -9,10 +8,22 @@ export function joinEventAction(userId, eventId) {
   return function(dispatch) {
     axios
       .post(`/api/v1/subscriptions/join/${userId}/${eventId}`)
-      .then(eventId => {
-        dispatch(getUsersByEvent(eventId)).then(() => {
-          alert("Joined");
-        });
+      .then(userId => {
+        return getUsersByEventApi(eventId);
+      })
+      .then(users => {
+        console.log("test", users);
+        dispatch(saveSelectedEventUsers(users));
+      })
+      .then(() => {
+        alert("Joined");
       });
+  };
+}
+
+function saveSelectedEventUsers(users) {
+  return {
+    type: "SAVE_SELECTED_EVENT_USERS",
+    users
   };
 }
