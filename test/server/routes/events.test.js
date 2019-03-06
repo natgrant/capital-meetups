a61062ce795debe04d2443497271b13a189a885c
ranjit03 <ranjitendurthi03@gmail.com>
2019-03-06 02:04:20 +0000

    all get routes tests done on events.test.js

--- test/server/routes/events.test.js
+++ test/server/routes/events.test.js
@@ -18,3 +18,43 @@ describe("Testing get event route by id", () => {
       .end(done);
   });
 });
+
+describe("Testing get events by creator route by id", () => {
+  test("Response needs to be a json", done => {
+    return request(app)
+      .get("/api/v1/meetups/creator/1")
+      .expect("Content-Type", /json/)
+      .end(done);
+  });
+});
+
+describe("Testing get events by category route by categoryname", () => {
+  test("Response needs to be a json", done => {
+    return request(app)
+      .get("/api/v1/meetups/category/Coffee")
+      .expect("Content-Type", /json/)
+      .end(done);
+  });
+});
+
+// describe('POST /users', function () {
+//   let data = {
+//       //no id
+//       "name": "dummy",
+//       "contact": "dummy",
+//       "address": "dummy"
+//   }
+//   it('respond with 400 not created', function (done) {
+//       request(app)
+//           .post('/users')
+//           .send(data)
+//           .set('Accept', 'application/json')
+//           .expect('Content-Type', /json/)
+//           .expect(400)
+//           .expect('"user not created"')
+//           .end((err) => {
+//               if (err) return done(err);
+//               done();
+//           });
+//   });
+// });
