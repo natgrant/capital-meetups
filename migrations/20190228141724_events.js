52c7aae80036537618fb9baa188b1be9ca4de1e9
Nat Grant <email.natgrant@gmail.com>
2019-03-07 00:20:28 +1300

    fix

--- migrations/20190228141724_events.js
+++ migrations/20190228141724_events.js
@@ -6,7 +6,7 @@ exports.up = function(knex, Promise) {
     table.string("location");
     table.string("description", 1000);
     table.string("category", 1000);
-    table.bigint("date", 1000);
+    table.bigint("date");
     table.boolean("is_open");
     table.string("type");
     table.string("image", 1000);
